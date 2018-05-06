d4c26b003b2d03007a8c2a2c550b68bf879fd11d
Uniaika <uniaika@crypto-keupone.eu>
2018-05-06 20:33:29 +0200

    global graphs

--- lib/fd_web/controllers/page_controller.ex
+++ lib/fd_web/controllers/page_controller.ex
@@ -1,5 +1,7 @@
 defmodule FdWeb.PageController do
   use FdWeb, :controller
+  
+  alias Fd.Instances
 
   def index(conn, _params) do
     render conn, "index.html"
@@ -13,6 +15,46 @@ defmodule FdWeb.PageController do
     conn
     |> assign(:title, "Instance Monitoring")
     |> render("monitoring.html")
+  end
+
+  def stats(conn, params) do
+    stats = get_global_stats(params)
+    render(conn, "stats.html", stats: stats)
+  end
+
+
+  defp get_global_stats(params) do
+    interval = "weekly"
+    stats         = Instances.get_global_statistics(interval)
+    get_serie = fn(stats, key) ->
+      Enum.map(stats, fn(stat) -> Map.get(stat, key, 0)||0 end)
+      |> Enum.reverse()
+    end
+    get_mg_serie = fn(stats, key) ->
+      stats
+      |> Enum.map(fn(stat) ->
+        value = Map.get(stat, key, 0)||0
+        date = Map.get(stat, "date")
+        %{"date" => date, "value" => value}
+      end)
+      |> Enum.reverse
+    end
+    %{
+      dates: get_serie.(stats, "date"),
+      users: get_serie.(stats, "users"),
+      statuses: get_serie.(stats, "statuses"),
+      peers: get_serie.(stats, "peers"),
+      emojis: get_serie.(stats, "emojis"),
+      mg_users: get_mg_serie.(stats, "users"),
+      mg_new_users: get_mg_serie.(stats, "new_users"),
+      mg_statuses: get_mg_serie.(stats, "statuses"),
+      mg_new_statuses: get_mg_serie.(stats, "new_statuses"),
+      mg_peers: get_mg_serie.(stats, "peers"),
+      mg_new_peers: get_mg_serie.(stats, "new_peers"),
+      mg_emojis: get_mg_serie.(stats, "emojis"),
+      mg_new_emojis: get_mg_serie.(stats, "new_emojis"),
+      interval: interval,
+    }
   end
 
 end
