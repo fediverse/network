50a8d9c2f26ffb6b4fbee1d2ee21bb242bb16515
Lukas Fülling <lukas@k40s.net>
2018-06-08 19:29:34 +0200

    skip email on maintenance, shorten toot, add intellij stuff do gitignore

--- lib/fd/email/down_email.ex
+++ lib/fd/email/down_email.ex
@@ -1,16 +1,21 @@
 defmodule Fd.DownEmail do
+  require Logger
   import Swoosh.Email
   import Fd.Util, only: [idna: 1]
 
   def down_email(instance = %Fd.Instances.Instance{}, check) do
-    new()
-    |> to({idna(instance.domain), instance.email})
-    |> from({"fediverse.network monitoring", "monitoring@email.fediverse.network"})
-    |> reply_to("root+instance-mon-#{instance.id}@fediverse.network")
-    |> put_bcc("root+instance-mon-#{instance.id}@fediverse.network")
-    |> subject("#{idna(instance.domain)} is down")
-    |> html_body(html(instance, check))
-    |> text_body(text(instance, check))
+    if instance.settings.maintenance_mode? do
+      Logger.info("Skipping down email for #{instance.domain} because maintenance is planned")
+    else
+      new()
+      |> to({idna(instance.domain), instance.email})
+      |> from({"fediverse.network monitoring", "monitoring@email.fediverse.network"})
+      |> reply_to("root+instance-mon-#{instance.id}@fediverse.network")
+      |> put_bcc("root+instance-mon-#{instance.id}@fediverse.network")
+      |> subject("#{idna(instance.domain)} is down")
+      |> html_body(html(instance, check))
+      |> text_body(text(instance, check))
+    end
   end
 
   def login(_), do: :error
