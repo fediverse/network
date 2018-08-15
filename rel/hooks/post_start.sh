eb52e328b3be5adf56348ce7a8bf5e5a34c9f2eb
href <href@random.sh>
2018-08-15 15:17:14 +0200

    fix releases

--- rel/hooks/post_start.sh
+++ rel/hooks/post_start.sh
@@ -1,13 +1,27 @@
-set +e
+#!/usr/bin/env bash
 
-echo "Starting app."
-echo "Waiting for node"
+set -o posix
+
+## Pings the running node, or an arbitrary peer node
+## by supplying `--peer` and `--cookie` flags.
+##
+## If the node is running and can be connected to,
+## 'pong' will be printed to stdout. If the node is
+## not reachable or cannot be connected to due to an
+## invalid cookie, 'pang' will be printed to stdout
+## and the command will exit with a non-zero status code.
+
+set -e
+
+require_cookie
+
+echo "Waiting for node."
 
 while true; do
-  nodetool ping > /dev/null
+  release_ctl ping --peer="$NAME" --cookie="$COOKIE" "$@" > /dev/null
   EXIT_CODE=$?
   if [ $EXIT_CODE -eq 0 ]; then
-    echo " up!"
+    echo " up."
     break
   fi
   echo -n "."
@@ -16,6 +30,10 @@ done
 set -e
 
 echo "-*- Running migrations"
-bin/fd rpc Elixir.Fd.ReleaseTasks migrate
+
+require_live_node
+
+release_remote_ctl rpc "Fd.ReleaseTasks.migrate()"
+
 echo "-*- Migrations run successfully"
 
